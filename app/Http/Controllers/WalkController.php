307092e0e9345791150597e1d3f0e55148e2a698
kbanchi1111 <kbanchi.1111@gmail.com>
2021-10-17 10:58:50 +0900

    install doctrine and add models

---
+++ app/Http/Controllers/WalkController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Walk;
+use Illuminate\Http\Request;
+
+class WalkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Walk  $walk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Walk $walk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Walk  $walk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Walk $walk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Walk  $walk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Walk $walk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Walk  $walk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Walk $walk)
+    {
+        //
+    }
+}
